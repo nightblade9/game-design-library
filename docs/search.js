8a57fd1d8374bf4a3f590b8161d489fa52618677
nightblade9 <nightbladecodes@gmail.com>
2021-02-14 09:03:44 -0500

    Update to latest Tropical with much smaller search.html page

--- docs/search.js
+++ docs/search.js
@@ -19,6 +19,19 @@ main = () => {
     }
 }
 
+// From https://stackoverflow.com/questions/247483/http-get-request-in-javascript/38297729#38297729
+httpGetAsync = (theUrl, callback) => {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+            callback(xmlHttp.responseText);
+        }
+    }
+
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.send(null); // ?!
+}
+
 onSearch = (searchQuery) => {
     if (searchQuery == null)
     {
@@ -30,101 +43,109 @@ onSearch = (searchQuery) => {
     document.getElementById("query").value = searchQuery;
 
     var searchWords = searchQuery.split(' ');
-    var allData = JSON.parse(window.data);
 
-    // Array. Each item is an "item" (data), then the score, then the order (pseudo-date).
-    // We sort by score descending, then by id/order ascending (older is better). Age 0 = older.
-    //
-    // This is deliberately to contrast the homepage and tags, where newer is better - we're
-    // trying to surface more/different things in search.
+    // Fetch data
+    var config = JSON.parse(window.config)
+    var siteRootUrl = config["siteRootUrl"];
+    var searchData = httpGetAsync(siteRootUrl + "/data.json", (searchData) => {
 
-    // title match is kinda important, tags match are meh, blurb match is super important.
-    // This is the point scoring, per keyword.
-    var weights = { "title": 2, "tags": 1, "blurb": 4 }
-    var everything = [];
+        // Parse data
+        var allData = JSON.parse(searchData);
 
-    // Search for matching tags, titles, and blurbs
-    for (var i = 0; i < allData.length; i++)
-    {
-        var item = allData[i];
+        // Array. Each item is an "item" (data), then the score, then the order (pseudo-date).
+        // We sort by score descending, then by id/order ascending (older is better). Age 0 = older.
+        //
+        // This is deliberately to contrast the homepage and tags, where newer is better - we're
+        // trying to surface more/different things in search.
 
-        everything.push({"item": item, "score": 0, "age": i}); // age 0 is older than age 1
-        
-        for (var k = 0; k < searchWords.length; k++)
+        // title match is kinda important, tags match are meh, blurb match is super important.
+        // This is the point scoring, per keyword.
+        var weights = { "title": 2, "tags": 1, "blurb": 4 }
+        var everything = [];
+
+        // Search for matching tags, titles, and blurbs
+        for (var i = 0; i < allData.length; i++)
         {
-            var searchWord = searchWords[k];
+            var item = allData[i];
+
+            everything.push({"item": item, "score": 0, "age": i}); // age 0 is older than age 1
             
-            // Do any words match any tag words?
-            // e.g. searching for "core" matches the tags "core" and "core loop" and "core-loop"
-            // e.g. searching for "core loop" matches the tags "core" and "core loop" and "loop stuff"
-            var tags = item["tags"];
-            for (var j = 0; j < tags.length; j++)
+            for (var k = 0; k < searchWords.length; k++)
             {
-                if (tags[j].toLowerCase().indexOf(searchWord.toLowerCase()) > -1)
+                var searchWord = searchWords[k];
+                
+                // Do any words match any tag words?
+                // e.g. searching for "core" matches the tags "core" and "core loop" and "core-loop"
+                // e.g. searching for "core loop" matches the tags "core" and "core loop" and "loop stuff"
+                var tags = item["tags"];
+                for (var j = 0; j < tags.length; j++)
                 {
-                    everything[i]["score"] += weights["tags"];
+                    if (tags[j].toLowerCase().indexOf(searchWord.toLowerCase()) > -1)
+                    {
+                        everything[i]["score"] += weights["tags"];
+                    }
+                }
+
+                // Does the word appear (possibly as a substring) in the blurb? We can use regex to make sure it's a word,
+                // but that might be against user expectations, and it would be slow if we have lots of long blurbs.
+                var blurb = item["blurb"].toLowerCase();
+                if (blurb.indexOf(searchWord.toLowerCase()) > -1)
+                {
+                    everything[i]["score"] += weights["blurb"];
+                }
+
+                // Finally, search the title.
+                var title = item["title"].toLowerCase();
+                if (title.indexOf(searchWord.toLowerCase()) > -1)
+                {
+                    everything[i]["score"] += weights["title"];
                 }
             }
-
-            // Does the word appear (possibly as a substring) in the blurb? We can use regex to make sure it's a word,
-            // but that might be against user expectations, and it would be slow if we have lots of long blurbs.
-            var blurb = item["blurb"].toLowerCase();
-            if (blurb.indexOf(searchWord.toLowerCase()) > -1)
-            {
-                everything[i]["score"] += weights["blurb"];
-            }
-
-            // Finally, search the title.
-            var title = item["title"].toLowerCase();
-            if (title.indexOf(searchWord.toLowerCase()) > -1)
-            {
-                everything[i]["score"] += weights["title"];
-            }
         }
-    }
 
-    // Sort by score descending, then age ascending (oldest first)
-    everything.sort((a, b) => a.score == b.score ? a.age > b.age : a.score < b.score)
-    
-    var finalHtml = "";
-    var config = JSON.parse(window.config)
-    var rootUrl = "siteRootUrl" in config ? config["siteRootUrl"] : ""
+        // Sort by score descending, then age ascending (oldest first)
+        everything.sort((a, b) => a.score == b.score ? a.age > b.age : a.score < b.score)
+        
+        var finalHtml = "";
+        var config = JSON.parse(window.config)
+        var rootUrl = "siteRootUrl" in config ? config["siteRootUrl"] : ""
 
-    var relevantCount = 0;
-    for (var i = 0; i < everything.length; i++)
-    {
-        var data = everything[i];
-        if (data["score"] > 0)
+        var relevantCount = 0;
+        for (var i = 0; i < everything.length; i++)
         {
-            relevantCount += 1;
-
-            var item = data["item"];
-            var tags = item["tags"];
-            var tagsHtml = "";
-            for (var j = 0; j < tags.length; j++)
+            var data = everything[i];
+            if (data["score"] > 0)
             {
-                // match everywhere we do tag normalization
-                var clean_tag = tags[j].replace(' ', '-').replace("'", "");
-                var proper_tag_name = tags[j].replace(/-/g, ' ');
-                tagsHtml += "<span class='tag'><a href='" + rootUrl + "/tags/" + clean_tag + ".html" + "'>" + proper_tag_name + "</a></span>"
-            }
-            
-            titleHtml = '<a href="' + item["url"] + '">' + item["title"] + "</a>"
+                relevantCount += 1;
 
-            blurb = item["blurb"] || ""
-            if ("icon" in item) {
-                titleHtml += "<img class='icon' src='" + rootUrl + "/images/" + item["icon"] + ".png' />"
+                var item = data["item"];
+                var tags = item["tags"];
+                var tagsHtml = "";
+                for (var j = 0; j < tags.length; j++)
+                {
+                    // match everywhere we do tag normalization
+                    var clean_tag = tags[j].replace(' ', '-').replace("'", "");
+                    var proper_tag_name = tags[j].replace(/-/g, ' ');
+                    tagsHtml += "<span class='tag'><a href='" + rootUrl + "/tags/" + clean_tag + ".html" + "'>" + proper_tag_name + "</a></span>"
+                }
+                
+                titleHtml = '<a href="' + item["url"] + '">' + item["title"] + "</a>"
+
+                blurb = item["blurb"] || ""
+                if ("icon" in item) {
+                    titleHtml += "<img class='icon' src='" + rootUrl + "/images/" + item["icon"] + ".png' />"
+                }
+                
+                finalHtml += window.snippet.replace("{title}", titleHtml)
+                    .replace("{url}", '<a href="' + item["url"] + '">' + item["url"] + "</a>")
+                    .replace("{tags}", tagsHtml)
+                    .replace("{blurb}", blurb);
             }
-            
-            finalHtml += window.snippet.replace("{title}", titleHtml)
-                .replace("{url}", '<a href="' + item["url"] + '">' + item["url"] + "</a>")
-                .replace("{tags}", tagsHtml)
-                .replace("{blurb}", blurb);
         }
-    }
-    
-    var header = "<h2>" + relevantCount + " items matching " + searchQuery + "</h2>";
-    setContent(header + finalHtml);
+        
+        var header = "<h2>" + relevantCount + " items matching " + searchQuery + "</h2>";
+        setContent(header + finalHtml);
+    });
 }
 
 main();
